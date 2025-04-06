13ebc4e1d52c35f80996132c7fc95c83cbbbccae
Fabian Merino <13384115+fabianmerino@users.noreply.github.com>
2025-04-06 07:14:38 -0500

    ✨ refactor: enhance user creation form and table structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { FormSubmitEvent, FormErrorEvent } from '@nuxt/ui'
+import type { FormSubmitEvent, FormErrorEvent, TableColumn } from '@nuxt/ui'
 import { z } from 'zod'
 import { useCreateUser, useFindManyUser } from '~~/lib/hooks';
 import { UserPrismaCreateSchema as schema } from '~~/lib/schemas/models'
@@ -11,7 +11,27 @@ const state = reactive<Partial<User>>({
   name: undefined,
   email: undefined,
 })
-
+const columns: TableColumn<User>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name'
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email'
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Created At',
+    cell: ({ row }) => {
+      return new Date(row.getValue('createdAt')).toLocaleString('es-PE')
+    }
+  },
+]
 const { mutate: createUser, isPending, isSuccess, isError } = useCreateUser()
 const { data: users, isFetching } = useFindManyUser({
   select: {
@@ -23,6 +43,8 @@ const { data: users, isFetching } = useFindManyUser({
   orderBy: {
     createdAt: 'desc',
   },
+}, {
+  initialData: [],
 })
 
 function onSubmit(payload: FormSubmitEvent<Partial<User>>) {
@@ -85,8 +107,8 @@ watch(
     <header class="text-center px-4 py-2.5 border-b border-(--ui-border)">
       <h1 class="font-bold">Welcome to the CRUD API</h1>
     </header>
-    <main class="mt-8 flex gap-4">
-      <UCard class="w-full max-w-xs">
+    <main class="mt-8 flex justify-between gap-4">
+      <UCard class="w-auto min-w-xs">
         <template #header>
           <h2 class="text-lg font-bold">Create User</h2>
           <p class="text-sm text-gray-500">Fill in the form below to create a new user.</p>
@@ -102,7 +124,7 @@ watch(
         </UForm>
       </UCard>
 
-      <UTable :loading="isFetching" :data="users" sticky class="flex-1 rounded-xl border border-(--ui-border)" />
+      <UTable :loading="isFetching" :columns :data="users" sticky class="flex-1 rounded-xl border border-(--ui-border)" />
     </main>
   </UContainer>
 </template>
